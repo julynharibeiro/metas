5c2dc9fd7b0ca1f228b032149647e53a631ba726
eu estive aquiiiii <111994458+julynharibeiro@users.noreply.github.com>
2024-03-21 08:17:00 -0300

    okk

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-conts botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
 
-conts textos = document.querySelectorAll(".
+for(let i=0; i<botoes.length; i++){
+    botoes[i].onclick = function(){
+        for(let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+
+            
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
