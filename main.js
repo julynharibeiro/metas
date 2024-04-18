46ec92489e48bd202ad831c0bbf74c8991335975
eu estive aquiiiii <111994458+julynharibeiro@users.noreply.github.com>
2024-04-18 08:34:07 -0300

    ok

--- main.js
+++ main.js
@@ -1,36 +1,10 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo")
-
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j=0; j<botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-
-            
-        }
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
-}
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-    }
-}
-
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2023-10-05T00:00:00");
+//constante da lista dos tempo de objetivo 
+const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
 function calculaTempo(tempoObjetivo) {
